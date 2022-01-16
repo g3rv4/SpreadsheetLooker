a33415bacbdb067f15e34a2b3163477480af578b
Gervasio Marchand <github@gervas.io>
2022-01-15 22:43:38 -0300

    use my package to build!

--- build.ps1
+++ build.ps1
@@ -1,32 +1,3 @@
-$basePath = Get-Location
-$publishPath = Join-Path $basePath .publish
-$packagesPath = Join-Path $basePath .packages
+Import-Module BuildScripts
 
-if (Test-Path $publishPath -PathType Container) {
-    rm -rf $publishPath
-}
-if (Test-Path $packagesPath -PathType Container) {
-    rm -rf $packagesPath
-} else {
-    New-Item -Path $packagesPath -ItemType "directory"
-}
-
-$uid = sh -c 'id -u'
-$gid = sh -c 'id -g'
-
-docker run --rm -v "$($basePath):/var/src" -v "$($publishPath):/var/publish" mcr.microsoft.com/dotnet/sdk:6.0.101-alpine3.14 ash -c "dotnet publish -c Release /var/src/SpreadsheetLooker.Web/SpreadsheetLooker.Web.csproj -o /var/publish && chown -R $($uid):$($gid) /var/publish"
-
-$version = [version](Get-Item "$publishPath/SpreadsheetLooker.Web.dll").VersionInfo.FileVersion
-$version = "$($version.Major).$($version.Minor).$($version.Build)"
-
-Write-Output "Version is $version"
-
-$nuspecPath = Join-Path $publishPath spreadsheetlooker.web.nuspec
-(Get-Content spreadsheetlooker.web.nuspec).Replace('$version$', $version) | Set-Content $nuspecPath
-
-$nupkgPath = Join-Path $packagesPath "spreadsheetlooker.web.$($version).nupkg"
-Compress-Archive -Path "$($publishPath)/*" -DestinationPath $nupkgPath
-
-if ($env:GITHUB_ENV) {
-    Write-Output "VERSION=$version" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
-}
+Build-BSDotNetPackage SpreadsheetLooker.Web mcr.microsoft.com/dotnet/sdk:6.0.101-alpine3.14
